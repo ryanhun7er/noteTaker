5f0087778d8c2d5774e1349863cb268782517d32
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-04-18 10:07:48 -0500

    port listening

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./public/assets/js/index")(app);
+
+// Sets up listener port
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
