dd68d67c3317898f186e4ba8418431d3be383547
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-04-21 14:42:23 -0500

    got the notes to write to json file, can't delete yet

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
+let db = require("./db/db.json");
 // Tells node that we are creating an "express" server
+
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port
+
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//const for reading and writing file sync
 
 const notes = JSON.parse(
   fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
@@ -29,8 +30,7 @@ const updateNote = notes => {
     JSON.stringify(notes),
     err => {
     if (err) throw err;
-    }
-);
+  });
 };
 
 // use app.get for linking html js and css files
@@ -66,22 +66,33 @@ app.post("/api/notes", (req, res) => {
   notes.push(addNote);
   updateNote(notes);
   return res.json(notes);
+
 });
 
 // process for deleting notes
 
 app.delete("/api/notes/:id", (req, res) => {
 
-  let id = req.params.id;
+  let noteid = JSON.parse(req.params.id);
+  let noteId2 = (req.originalUrl.replace(/\?.*$/, "")).split(":")[1];
 
-  delete notes[id - 1];
-  updateNote(notes);
-  res.send(notes);
+  for (let i=0; i < noteid.length; i++) {
+    if (noteid[i].id === noteId2) {
+      notes = []
+      noteid.splice(i, 1);
+      for (i=0; i < noteid.length; i++) {
+        notes.push(noteid[i]);
+        console.log(noteid);
+      }
+    }
+  }
+  fs.writeFileSync("db.json", JSON.stringify(notes));
+  return res.json(notes);
 
-})
 
 
 
+});
 
 
 
